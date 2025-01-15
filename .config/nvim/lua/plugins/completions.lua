3851b44bf5eafdb3a487691d09f204d5dc1c1455
AnoyingDog <188097811+AnoyingDog@users.noreply.github.com>
2025-01-15 15:39:19 +0100

    adding nvim

---
+++ .config/nvim/lua/plugins/completions.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "hrsh7th/cmp-nvim-lsp"
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          --scrolls_docs
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ["<C-CR>"] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, --For luasnip users.
+        }, {
+            { name = "buffer" },
+            { name = "path" },
+          }),
+      })
+    end,
+  },
+}
---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.opt.clipboard = "unnamedplus"
+vim.wo.relativenumber = true
+vim.opt.scrolloff = 7
+vim.opt.autoindent = true
+vim.opt.smartindent = true
